fd463b138b1c76f0388f7aaf9582583482a09c6b
Michael Hull <mike67hull@gmail.com>
2018-10-29 12:36:50 -0400

    Moshe Level

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,87 +1,74 @@
 <template>
-<div class="game">
+<div>
     <div class="alert alert-success">
-        Yay we have a game
+        Yay we have a game!
     </div>
+
     <div class="row">
         <div class="col-md-4">
-            <div class="card">
+            <div class="card" >
+                <div class="card-body">
+                    <h5 class="card-title">Players</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" >
+                <div class="card-body">
+                    <h5 class="card-title">My Captions</h5>
+                    <ul class="list-group list-group-flush">
+                        <li v-for="c in myCaptions" class="list-group-item">{{c}}</li>
+                    </ul>
+                  </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card" >
                 <img class="card-img" :src="state.picture.url" :alt="state.picture.name">
-                <div class="card-body">
-                  <h5 class="card-title">Flip Picture</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                 <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
+                <a @click.prevent="flipPicture" class="btn btn-primary">Flip Picture</a>
+
             </div>
         </div>
         <div class="col-md-4">
-            <div class="col-md-4">
-            <div class="card">
-                <img class="card-body" src="" alt="Card image cap">
+            <div class="card" >
                 <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                 <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title">Played Captions</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
-        </div>
-        </div>
-        <div class="col-md-4">
-            <div class="col-md-4">
-            <div class="card">
-                <img class="card-img" src="" alt="Card image cap">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                 <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        </div>
-        
-        <div class="col-md-4">
-            <div class="col-md-4">
-            <div class="card">
-                <img class="card-img" src="" alt="Card image cap">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                 <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
         </div>
     </div>
 </div>
 </template>
 
 <style lang="scss">
-
 </style>
 
 <script>
-import { GetState , FlipPicture, GetMyCaptions} from '@/services/api_access';
-
+import { GetState, FlipPicture, GetMyCaptions } from '@/services/api_access';
 export default {
     data: function(){
         return {
-           state: {
-               picture: "",
-               players: [],
-               playedCaptions = [],
-           },
-           myCaptions = [],
+            state: {
+                picture: "",
+                players: [],
+                playedCaptions: [],
+            },
+            myCaptions: [],
         }
     },
     created: function(){
-        GetState().then(x=> this.state = x);
-        GetMyCaptions().then(x=> this.myCaptions = x)
+        GetState()
+        .then(x=> this.state = x);
+        GetMyCaptions()
+        .then(x=> this.myCaptions = x);
     },
-    methods:{
+    methods: {
         flipPicture: function(){
             FlipPicture()
             .then(x=> GetState())
-            .then(x=> this.State = x)
+            .then(x=> this.state = x)
         }
     }
 }
