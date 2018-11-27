a17da0c6928e47419ddbf011da9ebcbd6f2569e9
Michael Hull <mike67hull@gmail.com>
2018-11-26 22:09:06 -0500

    Picture Picker

---
+++ src/components/PicturePicker.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <a  v-for="p in pictures" :key="p.id"
+            class="img-thumbnail">
+            <img :src="p.picture" />
+        </a>
+    </div>
+</template>
+<script>
+export default {
+    name: "PicturePicker",
+    data: function () {
+        return{
+        }
+    },
+    props: {
+        pictures: Array
+    }
+}
+</script>
+
+<style lang="scss">
+    .img-thumbnail {
+        display: inline-block;
+        margin: 5px;
+        width: 75px;
+        height: 75px;
+        text-align: center;
+        img {
+            max-width: 95%;
+            max-height: 95%;
+            border-radius: 3px;
+        }
+    }
+</style>
